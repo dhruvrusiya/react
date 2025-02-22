04fb0a18aec7f3f2ce3020903b34f0785e52024c
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-02-22 20:13:39 +0530

    react basic

---
+++ secondclass/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return ( <div>
+     <h1>hello chacha</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt laudantium quia, rem natus adipisci id quod praesentium quisquam harum totam!'</p>
+     <img src="insta photo.jpg" alt="" />
+     </div>
+  )
+}
+
+export default App
