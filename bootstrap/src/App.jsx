849d6ea542adb35bef96ac6ee510f12db27c1540
dhruv rusiya <dhruvrusiya070@gmail.com>
2025-03-10 19:59:48 +0530

    bootstrap

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,29 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+
+
+function App(){
+  return(
+    <>
+     
+
+
+    <Container>
+      <Row>
+        <Col>
+        <h1>first column</h1>
+        </Col>
+        <Col>
+       <h2>Second column</h2>
+       </Col>
+       <Col><h1>third column</h1>
+       </Col>
+       <Col>
+       <h1>fourth colomn
+        </h1></Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+export default App
